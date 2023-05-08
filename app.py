813a27b802cd8f4f0f103113fbb2a98885ab34df
JD Grillo <jdgrillo@microsoft.com>
2023-05-08 14:30:38 -0700

    fixing linting

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from flask import (
     url_for,
     send_from_directory,
 )
-from helpers.create_joke import create_joke
 from dotenv import load_dotenv
+from helpers.create_joke import create_joke
 
 load_dotenv()
 app = Flask(__name__)
